ffeaf63011ddc42486fceb3a0ce9efa5c3fa69ea
David <davidandresdiazgarcia@outlook.com>
2019-11-07 16:05:43 -0600

    cambie compañia

--- control_cheques/__manifest__.py
+++ control_cheques/__manifest__.py
@@ -8,7 +8,7 @@
     'description': """
     """,
 
-    'author': "My Company",
+    'author': "Xmarts",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
